b4e1240ec9b5b89602aa5d1d315f2467b283146e
mhassankhanw3 <mhassankhanw3@gmail.com>
2024-01-22 01:05:01 +0500

    bcrypted-password

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("application not able to talk to database", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server listening on port: ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed: " + err);
+  });
 
 // import express from "express";
 
@@ -11,10 +23,10 @@ connectDB();
 // (async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     app.on("error", (err) => {
-//       console.log("application not able to talk to database", err);
-//       throw err;
-//     });
+// app.on("error", (err) => {
+//   console.log("application not able to talk to database", err);
+//   throw err;
+// });
 //     app.listen(process.env.PORT, () => {
 //       console.log(`listening on port ${process.env.PORT}`);
 //     });
